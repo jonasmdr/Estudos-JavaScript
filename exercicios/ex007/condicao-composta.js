1912ed2af5b6d02e690f03eb828171cc7db9f495
Jonas Menezes <jonas.mdr.dev@gmail.com>
2026-06-10 15:08:21 -0300

    exercicio

--- exercicios/ex007/condicao-composta.js
+++ exercicios/ex007/condicao-composta.js
@@ -1,6 +1,6 @@
 let país = 'EUA'
 console.log(`Vivendo em ${país}`)
-if (país != 'Brasil') {
+if (país == 'Brasil') {
     console.log('Você é Brasileiro!')
 } else {
     console.log('Você é Estrangeiro')
