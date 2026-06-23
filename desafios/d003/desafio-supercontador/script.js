4ebd26efb1b38ff6ff8cc5cae6c79e6502ad116b
Jonas Menezes <jonas.mdr.dev@gmail.com>
2026-06-23 15:19:20 -0300

    desafio supercontador

---
+++ desafios/d003/desafio-supercontador/script.js
@@ -0,0 +1,22 @@
+let inicio = window.document.querySelector('input#inicio')
+let fim = window.document.querySelector('input#fim')
+let passo = window.document.querySelector('input#passo')
+
+function contar() {
+    let ini = Number(inicio.value)
+    let fi = Number(fim.value)
+    let pas = Number(passo.value)
+    let res = window.document.querySelector('div#res')
+    let num = document.createElement('p')
+    res.innerHTML = `Contando:`
+    if (ini == '' || fi == '' || pas == '') {
+        res.innerHTML = 'Impossível contar!'
+    } else if (pas <= 0) {
+            window.alert('Passo inválido! Considerando PASSO')
+        } else {
+    for (let inic = ini;inic <= fi;inic+=pas) {
+        num.innerHTML += ` \uD83D\uDC49 ${inic}`
+    }
+    res.appendChild(num)
+}
+}
