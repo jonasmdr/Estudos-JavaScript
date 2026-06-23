c78ce372ace8e3a788e4ddc63620f43c7bc98b51
Jonas Menezes <jonas.mdr.dev@gmail.com>
2026-06-23 12:37:00 -0300

    estrutura de repetições - while, do while e for

---
+++ exercicios/ex009/estrutura-de-repeticao-while-dowhile.js
@@ -0,0 +1,16 @@
+//Estrutura de repetição com teste lógico no inicio
+let c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+}
+
+//-----------------------------------------------------------
+
+//Estrutura de repetição com teste lógico no final
+
+let d = 10
+do {
+    console.log(`Passo ${d}`)
+    d++
+} while (d <= 20)
