7d5ee214e4e10528a8c39efd0ff0c42c77de0210
Jonas Menezes <jonas.mdr.dev@gmail.com>
2026-06-28 16:55:05 -0300

    Create metodos.js

---
+++ metodos.js
@@ -0,0 +1,15 @@
+let points = [
+    {x:0, y:0},
+    {x:1, y:1}
+];
+
+points.dist = function() {
+    let p1 = this[0];
+    let p2 = this[1];
+    let a = p2.x-p1.x;
+    let b = p2.y-p1.y;
+    return Math.sqrt(a*a+b*b);
+    
+};
+
+console.log(points.dist())
