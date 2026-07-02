c0c3b1bc47bb5a93da9df40dd3bf1cca40a35627
Jonas Menezes <jonas.mdr.dev@gmail.com>
2026-07-02 09:51:46 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -2,3 +2,29 @@ let num = 8;
 let $_u = 2;
 num **= 2
 console.log(num)
+
+soma = 0
+for(let n1 = 1;n1 <= 10; n1++) {
+    soma += n1
+}
+
+console.log(soma)
+
+let total = 0, count = 1;
+while (count <=10) {
+    total += count;
+    count += 1;
+}
+console.log(total);
+
+
+
+function factorial(n) {
+    if (n == 0) {
+        return 1;
+    } else {
+        return factorial(n - 1) * n;
+    }
+}
+
+console.log(factorial(5));
